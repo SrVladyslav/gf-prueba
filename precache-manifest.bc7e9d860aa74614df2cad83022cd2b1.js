1c05ce8374fefae640cff129e8f9aca9e6f943a1
Vladyslav <vlama@inf.upv.es>
2022-07-28 14:58:56 +0200

    deploy

---
+++ precache-manifest.bc7e9d860aa74614df2cad83022cd2b1.js
@@ -0,0 +1,370 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee208cc31ffb9dcc1b17",
+    "url": "gf-prueba/css/app.c45c306a.css"
+  },
+  {
+    "revision": "feaa8501c17b5d17bc97791ca6c9d00f",
+    "url": "gf-prueba/img/backround__landing.feaa8501.svg"
+  },
+  {
+    "revision": "03c69083d30ab33e4ae0cd32daaffa0c",
+    "url": "gf-prueba/img/blog.03c69083.svg"
+  },
+  {
+    "revision": "c82c8aad74a57b3ec280c59806c82d40",
+    "url": "gf-prueba/img/burger_activated.c82c8aad.svg"
+  },
+  {
+    "revision": "6b2959c33a6e43523ddd0c0e20bef46d",
+    "url": "gf-prueba/img/burger_deactivated.6b2959c3.svg"
+  },
+  {
+    "revision": "758b4dc24fae3c893d19f9b88d6474f6",
+    "url": "gf-prueba/img/catalonian.758b4dc2.svg"
+  },
+  {
+    "revision": "2c2719c08e9b22c33d230bee5a1bff96",
+    "url": "gf-prueba/img/chain.2c2719c0.svg"
+  },
+  {
+    "revision": "8075c8a61243a484c681a9dfe2b862b2",
+    "url": "gf-prueba/img/clock.8075c8a6.svg"
+  },
+  {
+    "revision": "c5dc6432df3379c0662eb91cd7222e64",
+    "url": "gf-prueba/img/coinbase__wallet.c5dc6432.png"
+  },
+  {
+    "revision": "0c9e1078c0f65e9048de92b4f145461a",
+    "url": "gf-prueba/img/collection.0c9e1078.svg"
+  },
+  {
+    "revision": "18e28570d56544f1b5118616946ea3b9",
+    "url": "gf-prueba/img/create.18e28570.svg"
+  },
+  {
+    "revision": "a897b68f82e8166505b7422587d073b9",
+    "url": "gf-prueba/img/cube.a897b68f.svg"
+  },
+  {
+    "revision": "83d04caf3c1f702ecae89ae24886dfe9",
+    "url": "gf-prueba/img/cube_r.83d04caf.svg"
+  },
+  {
+    "revision": "d2a712d27226ed82032c176649ee1528",
+    "url": "gf-prueba/img/dark_contract.d2a712d2.svg"
+  },
+  {
+    "revision": "e397b55de0fe6214a7ed9580ef04dd14",
+    "url": "gf-prueba/img/digest.e397b55d.png"
+  },
+  {
+    "revision": "145dc557845548a36a82337912ca3ac5",
+    "url": "gf-prueba/img/discord.145dc557.svg"
+  },
+  {
+    "revision": "1c8a54f25d101bdc607cec7228247a9a",
+    "url": "gf-prueba/img/discord.1c8a54f2.svg"
+  },
+  {
+    "revision": "961e93fca0e7d93aa89f6ec459e16b24",
+    "url": "gf-prueba/img/discord.961e93fc.png"
+  },
+  {
+    "revision": "b02a85343fa3ada70987fd6a953280db",
+    "url": "gf-prueba/img/dollar.b02a8534.svg"
+  },
+  {
+    "revision": "36ebb4e31c816d67109295f657d33c4a",
+    "url": "gf-prueba/img/english.36ebb4e3.svg"
+  },
+  {
+    "revision": "6f0e20c8a021ffd29d2538b8b8978f8c",
+    "url": "gf-prueba/img/error.6f0e20c8.svg"
+  },
+  {
+    "revision": "61ed4022faf3fe562eff91fc0c021592",
+    "url": "gf-prueba/img/ethereum.61ed4022.png"
+  },
+  {
+    "revision": "e339fcd1dddc3bf0c2babd604b7fabbb",
+    "url": "gf-prueba/img/ethereum_eth.e339fcd1.png"
+  },
+  {
+    "revision": "eefd4e6f6da9c05ac3e95aa2806562ab",
+    "url": "gf-prueba/img/filter.eefd4e6f.svg"
+  },
+  {
+    "revision": "63bb45bde462c6eb06669e68777d72e7",
+    "url": "gf-prueba/img/go_arrow.63bb45bd.svg"
+  },
+  {
+    "revision": "c8294870ec92e93ff2f62946453785f2",
+    "url": "gf-prueba/img/go_arrow_gray.c8294870.svg"
+  },
+  {
+    "revision": "0c2615a8e672139298efa728a0c9cb6b",
+    "url": "gf-prueba/img/go_arrow_left (1).0c2615a8.svg"
+  },
+  {
+    "revision": "72b8e91cd4404ffa7c0f2d22765cfb37",
+    "url": "gf-prueba/img/go_arrow_left.72b8e91c.svg"
+  },
+  {
+    "revision": "187f21b66451deba514c827e9d4bfbae",
+    "url": "gf-prueba/img/instagram.187f21b6.svg"
+  },
+  {
+    "revision": "3e19325add357fc10068127d2689d66c",
+    "url": "gf-prueba/img/instagram.3e19325a.png"
+  },
+  {
+    "revision": "e895cb69e22b9a2f3b1a592fc5dc6281",
+    "url": "gf-prueba/img/landing_bg.e895cb69.svg"
+  },
+  {
+    "revision": "8912ff7c5c0cdd448abbf1bc3d9d0864",
+    "url": "gf-prueba/img/lang.8912ff7c.svg"
+  },
+  {
+    "revision": "eaa4571f53fdc72416541935a5f34aff",
+    "url": "gf-prueba/img/lang.eaa4571f.svg"
+  },
+  {
+    "revision": "a7441ef29e839e7552f4bec5c6f6bea4",
+    "url": "gf-prueba/img/learn_more.a7441ef2.svg"
+  },
+  {
+    "revision": "fc347b856197eb400feec65036c9b0aa",
+    "url": "gf-prueba/img/link.fc347b85.png"
+  },
+  {
+    "revision": "7a77c76111e65fd10999a3ad1b460bd1",
+    "url": "gf-prueba/img/linkedin.7a77c761.png"
+  },
+  {
+    "revision": "97fc751fdec580eb9cf93eb766fc8b51",
+    "url": "gf-prueba/img/linkedin.97fc751f.svg"
+  },
+  {
+    "revision": "ddeaeae65310cdc8a419c5748820ddee",
+    "url": "gf-prueba/img/login.ddeaeae6.svg"
+  },
+  {
+    "revision": "0b00eefd6e6e8005f3d4d887541f5686",
+    "url": "gf-prueba/img/logo.0b00eefd.svg"
+  },
+  {
+    "revision": "20519336f47a9de6964906f5958073e7",
+    "url": "gf-prueba/img/logo.20519336.png"
+  },
+  {
+    "revision": "d6e98cb474f7215f4096195832ba1b0f",
+    "url": "gf-prueba/img/logo.png"
+  },
+  {
+    "revision": "09b729317f3c4d9d3de42e6dd0ec7834",
+    "url": "gf-prueba/img/logo_cube.09b72931.svg"
+  },
+  {
+    "revision": "8093902d72ee2712dd851081f288cc91",
+    "url": "gf-prueba/img/logo_vertical_blue.8093902d.svg"
+  },
+  {
+    "revision": "8a0a9ef67e5076b3588dbfc737008b49",
+    "url": "gf-prueba/img/logo_vertical_white.8a0a9ef6.svg"
+  },
+  {
+    "revision": "98cdeab06f37bccefbe98c22c445f666",
+    "url": "gf-prueba/img/logout.98cdeab0.svg"
+  },
+  {
+    "revision": "6262682a60cf47acb9fb1f6709419136",
+    "url": "gf-prueba/img/looksrare.6262682a.png"
+  },
+  {
+    "revision": "721cbf4aa2e5838b55f2790717cda51b",
+    "url": "gf-prueba/img/marketplace.721cbf4a.svg"
+  },
+  {
+    "revision": "7bafe373a1e2fec8f8381c3efabed673",
+    "url": "gf-prueba/img/medium.7bafe373.png"
+  },
+  {
+    "revision": "9ab7388ccdb708bb12407b0112d3e87a",
+    "url": "gf-prueba/img/metamask.9ab7388c.png"
+  },
+  {
+    "revision": "ce3375ab24300a3122108ba9a305be66",
+    "url": "gf-prueba/img/mode.ce3375ab.svg"
+  },
+  {
+    "revision": "69b10b0f7b40ffddbd7855475e40953d",
+    "url": "gf-prueba/img/old_create.69b10b0f.svg"
+  },
+  {
+    "revision": "47f8dfbf2c342447331b872e7a490ed0",
+    "url": "gf-prueba/img/old_lang.47f8dfbf.svg"
+  },
+  {
+    "revision": "a22fecd3e9409ebf282a78fd8ca24ebc",
+    "url": "gf-prueba/img/old_learn_more.a22fecd3.svg"
+  },
+  {
+    "revision": "27307b1bf84e2691441472b3a83b0b0b",
+    "url": "gf-prueba/img/old_marketplace.27307b1b.svg"
+  },
+  {
+    "revision": "45ab30902b51d53be3be975852260d16",
+    "url": "gf-prueba/img/old_mode.45ab3090.svg"
+  },
+  {
+    "revision": "dd6d5552ec0ee389ec314f012064a14d",
+    "url": "gf-prueba/img/old_profile.dd6d5552.svg"
+  },
+  {
+    "revision": "a2d79ec13535dc457dd72aefe0c3b693",
+    "url": "gf-prueba/img/old_wishes.a2d79ec1.svg"
+  },
+  {
+    "revision": "e1c5aae63ea365c4826fc840ae189084",
+    "url": "gf-prueba/img/opensea.e1c5aae6.png"
+  },
+  {
+    "revision": "e53fcc16e0fefe1ca3b132b24d58eac8",
+    "url": "gf-prueba/img/options.e53fcc16.svg"
+  },
+  {
+    "revision": "5f3aca623349b76555e54fc2fb3660e7",
+    "url": "gf-prueba/img/personalize__NFT.5f3aca62.svg"
+  },
+  {
+    "revision": "ee9fe9ac0573b69329cbdc1ffd066087",
+    "url": "gf-prueba/img/polygon.ee9fe9ac.png"
+  },
+  {
+    "revision": "ff47964ade8225d5b555242f49173979",
+    "url": "gf-prueba/img/polygon_eth.ff47964a.png"
+  },
+  {
+    "revision": "0074fee7e50052e61eb9017266b2b2bc",
+    "url": "gf-prueba/img/polygon_matic.0074fee7.png"
+  },
+  {
+    "revision": "324e144ad59565d638f1ade16b946ddc",
+    "url": "gf-prueba/img/profile.324e144a.svg"
+  },
+  {
+    "revision": "03bf79a6b187a604fbe48c7578a8ebdf",
+    "url": "gf-prueba/img/question.03bf79a6.svg"
+  },
+  {
+    "revision": "9227decb4b78e6d02f333137ff25d8bd",
+    "url": "gf-prueba/img/question_cancel.9227decb.svg"
+  },
+  {
+    "revision": "f022c58fef521ab23bc051f8e1325eda",
+    "url": "gf-prueba/img/rarible.f022c58f.png"
+  },
+  {
+    "revision": "3cd543e3288d4c7af0d05b9a1445cef1",
+    "url": "gf-prueba/img/reddit.3cd543e3.png"
+  },
+  {
+    "revision": "b4e99170f4046292c294d9725fc3971a",
+    "url": "gf-prueba/img/russian.b4e99170.svg"
+  },
+  {
+    "revision": "7f0f774646443d2db7379f58b65617aa",
+    "url": "gf-prueba/img/settings.7f0f7746.svg"
+  },
+  {
+    "revision": "d74609a5f300884ca87c2c97a7254349",
+    "url": "gf-prueba/img/share__NFT.d74609a5.svg"
+  },
+  {
+    "revision": "bf2d5a5f550531c2aeba9eeebb4300e7",
+    "url": "gf-prueba/img/spanish.bf2d5a5f.svg"
+  },
+  {
+    "revision": "75a0ec2885389445f481a0b3e5a16e8c",
+    "url": "gf-prueba/img/success.75a0ec28.svg"
+  },
+  {
+    "revision": "e2d9a21acd44fc539c2c94914c667014",
+    "url": "gf-prueba/img/sure.e2d9a21a.svg"
+  },
+  {
+    "revision": "8cf490d6cf88fd754d7fc1c3151cb011",
+    "url": "gf-prueba/img/sure_blue.8cf490d6.svg"
+  },
+  {
+    "revision": "4d49c2d8d5058c344e148baeb14225c7",
+    "url": "gf-prueba/img/telegram.4d49c2d8.png"
+  },
+  {
+    "revision": "7571ea13179d06d922f912f64d14abc6",
+    "url": "gf-prueba/img/twitter.7571ea13.png"
+  },
+  {
+    "revision": "db94b4c9e8a1766bb499de5c5aa13e87",
+    "url": "gf-prueba/img/twitter.db94b4c9.svg"
+  },
+  {
+    "revision": "b84583d7bebca30abd87481904bb45dd",
+    "url": "gf-prueba/img/ukrainian.b84583d7.svg"
+  },
+  {
+    "revision": "a52f3bb45930f1f513a55f95d0e68293",
+    "url": "gf-prueba/img/wallet.a52f3bb4.svg"
+  },
+  {
+    "revision": "f5df0667e3186f0951639e81616ca3b3",
+    "url": "gf-prueba/img/wallet.f5df0667.svg"
+  },
+  {
+    "revision": "f5a13f8d53dee135925cfc26d77a365d",
+    "url": "gf-prueba/img/wallet_ico.f5a13f8d.svg"
+  },
+  {
+    "revision": "eaedf9af6ddb83c5bce254c895a5fd38",
+    "url": "gf-prueba/img/wallet_mobile.eaedf9af.svg"
+  },
+  {
+    "revision": "6b22f91d12ba56a630d25b0179bcfaf8",
+    "url": "gf-prueba/img/website.6b22f91d.png"
+  },
+  {
+    "revision": "feb2d31d0fadb17edf3705fb15e732ef",
+    "url": "gf-prueba/img/wishes.feb2d31d.svg"
+  },
+  {
+    "revision": "fb6465b73b3ec6aac0c996d3cea497bc",
+    "url": "gf-prueba/img/woman.fb6465b7.svg"
+  },
+  {
+    "revision": "7d64ff16e2a572a3757f8f4f91bb619c",
+    "url": "gf-prueba/img/writer.7d64ff16.svg"
+  },
+  {
+    "revision": "0aef704404d11c06b630e82d6f3c0e0a",
+    "url": "gf-prueba/index.html"
+  },
+  {
+    "revision": "ee208cc31ffb9dcc1b17",
+    "url": "gf-prueba/js/app.2aeda815.js"
+  },
+  {
+    "revision": "28507b5ff8d6cb20c098",
+    "url": "gf-prueba/js/chunk-vendors.4b2617a5.js"
+  },
+  {
+    "revision": "693d2b275935e6007d335a8cf9cc599d",
+    "url": "gf-prueba/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "gf-prueba/robots.txt"
+  }
+]);
